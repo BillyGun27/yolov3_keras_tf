4daf0a724ee6b01576cf138950764ee0fce5a69e
billygun27 <billygun27@gmail.com>
2019-05-02 09:34:10 +0700

    rand

--- utils/train_tool.py
+++ utils/train_tool.py
@@ -136,3 +136,28 @@ def data_generator_wrapper_double(annotation_lines, batch_size, input_shape, anc
     n = len(annotation_lines)
     if n==0 or batch_size<=0: return None
     return data_generator_double(annotation_lines, batch_size, input_shape, anchors, num_classes)
+
+def data_generator_rand(annotation_lines, batch_size, input_shape, anchors, num_classes):
+    '''data generator for fit_generator'''
+    n = len(annotation_lines)
+    i = 0
+    while True:
+        image_data = []
+        box_data = []
+        for b in range(batch_size):
+            if i==0:
+                np.random.shuffle(annotation_lines)
+            image, box = get_random_data(annotation_lines[i], input_shape, random=True)
+            image_data.append(image)
+            box_data.append(box)
+            i = (i+1) % n
+        image_data = np.array(image_data)
+        box_data = np.array(box_data)
+        y_true = preprocess_true_boxes(box_data, input_shape, anchors, num_classes)
+        l_true =  [ np.random.rand(  batch_size ,416//{0:32, 1:16, 2:8}[l], 416//{0:32, 1:16, 2:8}[l], 9//3, 20+5 ) for l in range(3) ]
+        yield [image_data, *y_true, *l_true], np.zeros(batch_size)
+
+def data_generator_wrapper_rand(annotation_lines, batch_size, input_shape, anchors, num_classes):
+    n = len(annotation_lines)
+    if n==0 or batch_size<=0: return None
+    return data_generator_rand(annotation_lines, batch_size, input_shape, anchors, num_classes)
